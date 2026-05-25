218d8e8b172fc955e02d0085a1d9d2afd33cddf2
Rajat Kumar Dash <119514152+qm-rajat@users.noreply.github.com>
2026-05-25 10:39:57 +0000

    y

--- app/login/page.js
+++ app/login/page.js
@@ -1,6 +1,6 @@
 'use client'
 '// app/login/page.js'
-import { useState, Suspense } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { MdTableRestaurant } from 'react-icons/md'
